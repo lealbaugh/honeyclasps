dcc8ebc808c2f4780d226edb630522575e1132f0
lealbaugh <l.e.albaugh@gmail.com>
2014-03-29 23:05:34 -0400

    Voronoi pipes with attenuation.

--- honey.py
+++ honey.py
@@ -2,6 +2,8 @@ import rhinoscriptsyntax as rs
 import random
 import voronoi
 
+vertex_radius = 1.5
+
 # The voronoi module wants its points as objects, instead of just tuples
 class Point(object):
 	def __init__(self, x=0, y=0, z=0):
@@ -9,20 +11,31 @@ class Point(object):
 		self.y = y
 		self.z = z
 
+def lerp(value, inputLow, inputHigh, outputLow, outputHigh):
+	return (((value-inputLow)/(inputHigh-inputLow))*(outputHigh-outputLow))+outputLow
+
 points = []
 for i in range(30):
 	point = Point(random.randrange(30), random.randrange(30), 0)
 	rs.AddPoint(point.x, point.y, point.z)
 	points.append(point)
 results = voronoi.computeVoronoiDiagram(points)
+
 for edge in results[2]:
 	if edge[1] != -1 and edge[2] != -1:
 		startx = results[0][edge[1]][0]
 		starty = results[0][edge[1]][1]
 		endx = results[0][edge[2]][0]
 		endy = results[0][edge[2]][1]
-		rs.AddLine((startx, starty, 0), (endx, endy, 0))
-
+		curve = rs.AddLine((startx, starty, 0), (endx, endy, 0))
+		curvelength = rs.Distance((startx, starty, 0), (endx, endy, 0))
+		attenuation = lerp(curvelength, 0.0, 20.0, vertex_radius, vertex_radius/3)
+		if attenuation < vertex_radius/3:
+			attenuation = vertex_radius/3
+		# rhinoscriptsyntax.AddPipe (curve_id, parameters, radii, blend_type=0, cap=0, fit=False)
+		rs.AddPipe(curve, (0,0.5,1), (vertex_radius, attenuation, vertex_radius), fit=True)
+for vertex in results[0]:
+	rs.AddSphere((vertex[0],vertex[1],0), vertex_radius)
 
 	#        Returns a 3-tuple of:
 	#
