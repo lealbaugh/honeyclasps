53df9ef6a915c5a4c12e49db5e5b4367a7c3545e
lealbaugh <l.e.albaugh@gmail.com>
2014-04-03 12:31:15 -0400

    Modularization.

--- honey.py
+++ honey.py
@@ -23,7 +23,8 @@ def decimate(input):
 	# output a Decimal at rhino-compatible amounts of precision
 	return Decimal(input).quantize(Decimal('1.000'))
 
-def makePipesFromVoronoi(results):
+def plotVoronoi(results):
+	curves = []
 	for edge in results[2]:
 		if edge[1] != -1 and edge[2] != -1:
 			startx = decimate(results[0][edge[1]][0])
@@ -32,39 +33,42 @@ def makePipesFromVoronoi(results):
 			endy = decimate(results[0][edge[2]][1])
 			startpoint = [startx, starty, 0]
 			endpoint = [endx, endy, 0]
-			curvelength = rs.Distance(startpoint, endpoint)
-			if curvelength < 25:
-				drawPipe(startpoint, endpoint, curvelength)
-	# for vertex in results[0]:
-	# 	rs.AddSphere((vertex[0],vertex[1],0), vertex_radius)
+			curves.append(rs.AddLine(startpoint, endpoint))
+	return curves
 
-def makePipesFromDelauney(points, results):
+def plotDelauney(points, results):
+	curves = []
 	for result in results:
-		drawPipe(points[result[0]].x, points[result[0]].y, points[result[1]].x, points[result[1]].y)
-		drawPipe(points[result[1]].x, points[result[1]].y, points[result[2]].x, points[result[2]].y)
-		drawPipe(points[result[2]].x, points[result[2]].y, points[result[0]].x, points[result[0]].y)
-	for vertex in points:
-		rs.AddSphere((vertex.x,vertex.y,0), vertex_radius)
+		curves.append(rs.AddLine(points[result[0]].x, points[result[0]].y, points[result[1]].x, points[result[1]].y))
+		curves.append(rs.AddLine(points[result[1]].x, points[result[1]].y, points[result[2]].x, points[result[2]].y))
+		curves.append(rs.AddLine(points[result[2]].x, points[result[2]].y, points[result[0]].x, points[result[0]].y))
+	return curves
 
+def makeCurvesBlobby(curves, vertex_radius):
+	existingsegments = []
+	for curve in curves:
+		curvelength = rs.CurveLength(curve)
+		if curvelength < 25:
+			attenuation = lerp(curvelength, 0.0, 20.0, vertex_radius-vertex_radius/6, vertex_radius/2)
+			if attenuation < vertex_radius/3:
+				attenuation = vertex_radius/3
+			# rhinoscriptsyntax.AddPipe (curve_id, parameters, radii, blend_type=0, cap=0, fit=False)
+			pipe = rs.AddPipe(curve, (0,0.5,1), (vertex_radius, attenuation, vertex_radius), fit=True)
+			startsphere = rs.AddSphere(rs.CurveStartPoint(curve), vertex_radius)
+			endsphere = rs.AddSphere(rs.CurveEndPoint(curve), vertex_radius)
+			if pipe and startsphere and endsphere:
+				pipe = rs.BooleanUnion([startsphere, pipe, endsphere], True)
+	web = rs.JoinSurfaces(existingsegments, True)
+	return web
 
-def drawPipe(startpoint, endpoint, curvelength):
-	curve = rs.AddLine(startpoint, endpoint)
-	attenuation = lerp(curvelength, 0.0, 20.0, vertex_radius-vertex_radius/6, vertex_radius/2)
-	if attenuation < vertex_radius/3:
-		attenuation = vertex_radius/3
-	# rhinoscriptsyntax.AddPipe (curve_id, parameters, radii, blend_type=0, cap=0, fit=False)
-	pipe = rs.AddPipe(curve, (0,0.5,1), (vertex_radius, attenuation, vertex_radius), fit=True)
-	startsphere = rs.AddSphere(startpoint, vertex_radius)
-	endsphere = rs.AddSphere(endpoint, vertex_radius)
-	if pipe and startsphere and endsphere:
-		pipe = rs.BooleanUnion([startsphere, pipe, endsphere], True)
-	return pipe
+def randomPoints(quantity, area):
+	points = []
+	for i in range(quantity):
+		point = Point(random.randrange(area), random.randrange(area), 0)
+		rs.AddPoint(point.x, point.y, point.z)
+		points.append(point)
+	return points
 
-points = []
-for i in range(30):
-	point = Point(random.randrange(20), random.randrange(20), 0)
-	# rs.AddPoint(point.x, point.y, point.z)
-	points.append(point)
-
-makePipesFromVoronoi(voronoi.computeVoronoiDiagram(points))
-# makePipesFromDelauney(points, voronoi.computeDelaunayTriangulation(points))
+plotVoronoi(voronoi.computeVoronoiDiagram(randomPoints(30,20)))
+curves = rs.GetObjects ( message="Select curves to be blobbified...", filter=4)
+makeCurvesBlobby(curves, 1.5)
